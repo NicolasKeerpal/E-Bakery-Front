d3fac61754e5ccc24db0214f76c16680643123a1
Yann <yann.sady.pro@gmail.com>
2024-05-20 23:44:13 +0200

    🚧 begin developpement of our-products page

--- src/app/components/our-products/our-products.component.ts
+++ src/app/components/our-products/our-products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FoodService } from '../../services/food.service';
+import { Food } from '../../models/food';
 
 @Component({
   selector: 'app-our-products',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './our-products.component.scss'
 })
 export class OurProductsComponent {
+  foods: Food[] = [];
 
+  constructor(private foodService: FoodService) { }
+
+  ngOnInit(): void {
+    this.foodService.getFoods().subscribe(
+      (data: Food[]) => {
+        this.foods = data;
+        console.log('foods : ', this.foods); 
+      },
+      error => {
+        console.error('Erreur lors de la récupération des aliments', error);
+      }
+    );
+  }
 }
